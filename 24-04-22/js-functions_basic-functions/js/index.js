aa1a54de5c63da38f17a3775f39227961d3eea51
Rebecca Voll <doktavoll@gmail.com>
2024-04-22 12:56:13 +0200

    solve JS functions challenges

---
+++ 24-04-22/js-functions_basic-functions/js/index.js
@@ -0,0 +1,88 @@
+console.clear();
+
+// DONE
+
+/*
+1: Create the data for a book in an online store. Create variables for the following data:
+  - the book title
+  - the author
+  - the rating
+  - the number of sales
+*/
+
+// --v-- write your code here --v--
+const bookTitle = "The Lord of the Javascript";
+const author = "Mario";
+let rating = 4.2;
+let numberOfSales = 120;
+
+// --^-- write your code here --^--
+
+/*
+2: Log all variables to the console, for example:
+
+Title: The Lord of the Javascript
+Author: Mario
+Rating: 4.2
+Sales: 120
+
+Increase the number of sales and change the rating of the book afterwards.
+Then log all variables to the console again.
+*/
+
+// --v-- write your code here --v--
+
+console.log("Title: " + bookTitle);
+console.log("Author: " + author);
+console.log("Rating:" + rating);
+console.log("Sales: " + numberOfSales);
+
+numberOfSales++;
+rating += 0.1;
+
+// console.log("Title: " + bookTitle);
+// console.log("Author: " + author);
+// console.log("Rating:" + rating);
+// console.log("Sales: " + numberOfSales);
+//
+// und hier ersetzt durch die Funktion aus 3.:
+logBookData();
+
+// --^-- write your code here --^--
+
+/*
+3: This code is quite repetitive and hard to read.
+ - Write a function called logBookData which console logs all
+   variables when it is called.
+ - Then replace the console logs above with this function.
+ - Now increase the number of sales 2 more times and log the data after each increase.
+*/
+
+// --v-- write your code here --v--
+
+function logBookData() {
+  console.log("Title: " + bookTitle);
+  console.log("Author: " + author);
+  console.log("Rating:" + rating);
+  console.log("Sales: " + numberOfSales);
+}
+// hier nochmal ein Versuch, alles in ein console.log zu kriegen a la Isabell:
+// function logBookData() {
+//   console.log("Title: " + bookTitle "; " +
+// "Author: " + author "; "
+// "Rating:" + rating "; "
+// "Sales: " + numberOfSales "; ")
+// }
+
+numberOfSales++;
+
+logBookData();
+
+numberOfSales++;
+
+logBookData();
+
+numberOfSales++;
+
+logBookData();
+// --^-- write your code here --^--
