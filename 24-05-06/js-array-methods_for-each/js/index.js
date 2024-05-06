1901e1c1ec26b3ea03c957dbb47fc7dacaf86f35
Rebecca Voll <doktavoll@gmail.com>
2024-05-06 12:57:04 +0200

    work on index.js and solve JS Array methods 1 challenge 1

--- 24-05-06/js-array-methods_for-each/js/index.js
+++ 24-05-06/js-array-methods_for-each/js/index.js
@@ -19,11 +19,95 @@ const colors = [
   "#A5A5A5",
 ];
 
+// Task 1:
+
 colors.forEach((color) => {
   const colorElement = document.createElement("div");
+  // // just4fun:
+  // colorElement.textContent = color;
+
+  colorElement.classList.add("color-box");
+
+  // diese Zeile ist von Rahaf; weiß noch nicht, wie sie draufgekommen ist; selber ausprobieren!
+  // das kommt von css
+  // styles kann man anpassen und dann mit "." und camelCase parameter modifizieren
+  colorElement.style.backgroundColor = color;
+
+  document.body.append(colorElement);
+});
+
+// Task 2:
+
+function renderColorBox(color) {
+  const colorElement = document.createElement("div");
+
   // just4fun:
   colorElement.textContent = color;
 
-  div.classList.add(color - box);
+  colorElement.classList.add("color-box");
+
+  colorElement.style.backgroundColor = color;
+
   document.body.append(colorElement);
-});
+}
+
+colors.forEach(renderColorBox);
+
+// for each bezieht sich auf das was davor kommt, nicht was ihm folgt (counterintuitive)
+// lies: for each of "colors" aplly function "renderColorBox" oder
+// group of elements - for each memebr of group of elements - carry out function "render color box"
+// die Reihenfolge von Funktion und forEach"phrase" ist egal, aber sollte per Konvention immer gleich sein.
+
+// das war die Aufgabe: eine Fkt.
+// für eine Box schreiben und die dann auf alle anwenden;
+// eine andere mögliche aber hier nicht gefragte Version ist Isabells, nämlich,
+//
+//
+// das hier funktioniert nur mit dem Anfang (Task1):
+// renderColorBox.forEach(colors);
+
+// Isabells:
+// const renderColorBox = (colors) => {
+//   colors.forEach((color) => {
+//     const box = document.createElement("div");
+//     box.classList.add("color-box");
+//     box.style.backgroundColor = color;
+//     document.body.append(box);
+//   });
+// };
+// renderColorBox(colors);
+
+// ab hier alles auskommentiert:
+// // Task 2:
+// // Rahafs Lösung; ich habs noch nicht probiert
+// //
+// function renderColorbox(color) {
+//   // kopiert:
+//   const colorElement = document.createElement("div");
+//   // // just4fun:
+//   // colorElement.textContent = color;
+
+//   colorElement.classList.add("color-box");
+
+//   colorElement.style.backgroundColor = color;
+
+//   document.body.append(colorElement);
+// }
+
+// colors.forEach(renderColorBox);
+
+// // // oder Isabell style, aber funktioniert noch ncht richtig:
+// // renderColorbox.forEach((color) => {
+
+// //   // kopiert:
+// //   const colorElement = document.createElement("div");
+// //   // // just4fun:
+// //   // colorElement.textContent = color;
+
+// //   colorElement.classList.add("color-box");
+
+// //   colorElement.style.backgroundColor = color;
+
+// //   document.body.append(colorElement);
+
+// // });
