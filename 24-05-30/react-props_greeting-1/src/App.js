bc0938f28eeba736a3c1ae26ea59349b6a632ccf
Rebecca Voll <doktavoll@gmail.com>
2024-06-07 22:57:49 +0200

    modify react-props-greeting-1 solving the challenge more or less on my own, credit to chat GPT and mdn

--- 24-05-30/react-props_greeting-1/src/App.js
+++ 24-05-30/react-props_greeting-1/src/App.js
@@ -1,5 +1,33 @@
 import "./styles.css";
 
+const name = "Buddy";
+
+// - Inside of the return statement of your `App` component,
+// replace the heading with your `Greeting` component and
+// pass it a `name` prop with a value of your choice.
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  console.log("name: ", { name });
+
+  return <Greeting name={name} />;
+  // the line above is what I got wrong;
+  // it passes the 'name' prop to the Greeting component!
+  // I had just written 'return Greeting();'
+
+  // Why does it not say anywhere in the handout,
+  // what the (return statement in the) export function should look like?
+  // as in '<Function key={prop}/>'?
+  // should I know this from an earlier Handout?
+}
+
+// - Write the `Greeting` component inside of the `src/App.js`.
+
+// - It accepts a prop called `name` (make sure to destructure it).
+function Greeting({ name }) {
+  return (
+    <div>
+      <h1>Hello {name}!</h1>
+    </div>
+  );
+  // - It returns an HTML element and uses the `name` prop to render "Hello, [name]!";
 }
