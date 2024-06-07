6fa6ffb61e00e142ac50f233346d2e8109838f9a
Rebecca Voll <doktavoll@gmail.com>
2024-06-07 20:03:11 +0200

    modify React basic challenge Hello world article

--- 24-05-29/react-basics_hello-world-article/src/App.js
+++ 24-05-29/react-basics_hello-world-article/src/App.js
@@ -5,11 +5,6 @@ export default function App() {
   return HelloWorldArticle();
 }
 
-//
-// Render an HTML article tag containing a heading
-// and a paragraph to the browser. To do so,
-// create a component called HelloWorldArticle.
-//
 function HelloWorldArticle() {
   return (
     <article>
@@ -20,27 +15,3 @@ function HelloWorldArticle() {
     </article>
   );
 }
-
-// Use the following hints as guideline:
-
-// In the App.js, create a new function called HelloWorldArticle().
-// HelloWorldArticle() should return an HTML article tag as a wrapper which contains;
-// an h1 heading
-// at least one p tag
-// Note: Add whatever tags you like, but the predefined styles are limited ;)
-// In the return statement of your App function, replace the div with your HelloWorldArticle component.
-
-// hierunter vorgegebene Lösung von jd. anders'/oder meine alte?
-// Egal. Ich mach das jetzt nochmal!
-//
-// 1.
-// function HelloWorldArticle() {
-//   return (
-//     <wrapper>
-//       <h1>This is the title</h1>
-//       <p>This is the first paragraph.</p>
-//       <p>This is the second paragraph.</p>
-//       <p>This is the third paragraph.</p>
-//     </wrapper>
-//   );
-// }
