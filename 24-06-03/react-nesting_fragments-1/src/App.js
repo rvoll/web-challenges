803d9fac4d61fe907cd7dd47c3a8f36a63fd46a8
Rebecca Voll <doktavoll@gmail.com>
2024-06-08 23:08:59 +0200

    modify react-nesting_fragments-1, solving the challenge

--- 24-06-03/react-nesting_fragments-1/src/App.js
+++ 24-06-03/react-nesting_fragments-1/src/App.js
@@ -3,11 +3,33 @@ import "./styles.css";
 import Box from "./components/Box";
 
 export default function App() {
+  function Boxes() {
+    return (
+      <>
+        <Box color="#007bff" />
+        <Box color="#fc3" />
+        <Box color="#ff3333" />
+      </>
+    );
+  }
+
   return (
     <main className="flex-container">
-      <Box color="#007bff" />
-      <Box color="#fc3" />
-      <Box color="#ff3333" />
+      <Boxes />
     </main>
   );
 }
+
+// Anm. (s. README):
+// sometimes it is necessary to use fragments
+// rather than a div tag;
+// in this challenge for example, when using <div>
+// instead of <> (=fragments)
+// in the return statement of the Boxes component,
+// the boxes are vertically aligned! 😱
+
+// The reason is this: a flexbox refers only
+// to its direct child elements;
+// however, using a <div> element in Boxes
+// creates a new layer so that the three Box
+// elements are no longer flex items!
