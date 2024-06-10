22921f3f4d517c9bed667512c84a2f8e1b0b6023
Rebecca Voll <doktavoll@gmail.com>
2024-06-10 11:51:21 +0200

    clone web demos and modify Pokémon API challenge files

--- 24-06-10/react-effects-and-fetch_pokemon-api/src/components/PokemonList.js
+++ 24-06-10/react-effects-and-fetch_pokemon-api/src/components/PokemonList.js
@@ -1,23 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  // Questions:
+  // why empty array above in useState
+  // and why an empty array below, as argument of useEffect?
 
+  console.log("pokemon: ", pokemon);
+
+  // What does the function loadPokemon below do? Is it only for error handling? see HAndout JS error handling
+  // "async function loadPokemon() {try {x} catch () {y}"}
+  // it is used below in the useEffect function.
   async function loadPokemon() {
     try {
       const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+
       const data = await response.json();
+
       setPokemon(data.results);
     } catch (error) {
       console.log(error);
     }
   }
 
+  // Einbau des useEffect Hook um Pokemon automatisch beim Seite laden zu ?fetchen,
+  // mit leerem dependency array (Q: warum leer?)
+  // useEffect Hook:
+  // useEffect(() => {  }, [ ] )
+
+  useEffect(() => {
+    loadPokemon();
+  }, []);
+
   return (
     <main>
-      <button type="button" onClick={loadPokemon}>
-        Load Pokémon
-      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
@@ -26,3 +42,13 @@ export default function PokemonList() {
     </main>
   );
 }
+// Aufgabe:
+
+//Change the code below so that
+// the loadPokemon function is only executed
+// when the app component is rendered initially`
+
+// unter main rausgenommen:
+// <button type="button" onClick={loadPokemon}>
+//        Load Pokémon
+//        </button>
