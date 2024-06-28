01323d548c35fe2a357a0f6e84c713158a07513d
Rebecca Voll <doktavoll@gmail.com>
2024-06-28 11:09:44 +0200

    rename directories

--- Cities-of-the-World-App_SelfDirLearn_24-06-28/cities/pages/cities/index.js
+++ Cities-of-the-World-App_SelfDirLearn_24-06-28/cities/pages/cities/index.js
@@ -1,1 +1,10 @@
 // This is where we will show a list of all cities.
+
+// To do that, import the cities from our data.js file
+// (be sure to pay attention to the path to the file).
+
+// Once you have the data,
+// loop over each city object
+// and create an li element.
+
+// For now, just display the cities
