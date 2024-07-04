494a8eb3bb27d416c5357385a298b097aa4767a7
Rebecca Voll <doktavoll@gmail.com>
2024-07-04 11:23:20 +0200

    changes in 07-03_React-Global-State/react-global-state_lights

--- 07-03_React-Global-State/react-global-state_lights/pages/_app.js
+++ 07-03_React-Global-State/react-global-state_lights/pages/_app.js
@@ -1,6 +1,8 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
 
+// In this challenge you will automate the lights in your house… well, at least in your browser.
+
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
