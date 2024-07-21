df0b6e640ead866248625a9294c849c7e57def4f
Rebecca Voll <doktavoll@gmail.com>
2024-07-21 16:16:15 +0200

    slightly modify server.js

--- 07-10_backend-basics/backend-basics_routes/server.js
+++ 07-10_backend-basics/backend-basics_routes/server.js
@@ -3,7 +3,6 @@ import { createServer } from "node:http";
 export const server = createServer((request, response) => {
   if (request.url === "/api/fish/1") {
     response.statusCode = 200;
-
     response.end("Shrimp");
   } else if (request.url === "/api/fish/2") {
     response.statusCode = 200;
