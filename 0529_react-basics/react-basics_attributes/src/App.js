77b7e251a748b47b97b235428eccd69476685ae9
Rebecca Voll <doktavoll@gmail.com>
2024-08-02 22:44:58 +0200

    create an article component with the requested elements and render it in the App() function in 0529/react-basics_attributes/src/App.js

--- 0529_react-basics/react-basics_attributes/src/App.js
+++ 0529_react-basics/react-basics_attributes/src/App.js
@@ -2,5 +2,29 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article_title">some title</h2>
+      <label id="input" htmlFor="input">
+        fill in whatever you want:
+      </label>
+      <input id="input" htmlFor="input"></input>
+      <a
+        className="article_link"
+        href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element"
+      >
+        mdn&apos;s list of HTML elements
+      </a>
+      <a
+        className="article_link"
+        href="https://react.dev/learn/writing-markup-with-jsx"
+      >
+        React&apos;s explanation of JSX
+      </a>
+    </article>
+  );
 }
