0360837c59d1602e863d38542f1eae1955b4f5ba
Rebecca Voll <doktavoll@gmail.com>
2024-08-02 22:14:16 +0200

    slightly modify web-challenges/0529/react-basics_hello-world-article/src/App.js

--- 0529_react-basics/react-basics_hello-world-article/src/App.js
+++ 0529_react-basics/react-basics_hello-world-article/src/App.js
@@ -10,32 +10,32 @@ function HelloWorldArticle() {
     <article>
       <h1>We are a bunch of nested articles</h1>
       <article>
-        <h2>I´m number one - the biggest</h2>
-        <h3>I´m the number one paragraph - the biggest.</h3>
+        <h2>I&apos;m number one - the biggest</h2>
+        <h3>I&apos;m the number one paragraph - the biggest.</h3>
         <p>
           I contain a few lines of meaningless text. Yes I do. Yes I do. Yes I
           do. Yes I do. Yes I do. Yes I do. Yes I do. Yes I do.{" "}
         </p>
-        <h3>I´m number two</h3>
+        <h3>I&apos;m number two</h3>
         <p>
           So do I. Yes I do. Yes I do. Yes I do. Yes I do. Yes I do. Yes I do.
           Yes I do. Yes I do.
         </p>
-        <h3>And I´m number three</h3>
+        <h3>And I&apos;m number three</h3>
         <p>
           Mee, too. Yes I do. Yes I do. Yes I do. Yes I do. Yes I do. Yes I do.
           Yes I do. Yes I do.
         </p>
       </article>
       <article>
-        <h2>I´m the number two nested article</h2>
+        <h2>I&apos;m the number two nested article</h2>
         <p>
           I also contain a few lines of meaningless text. Yes I do. Yes I do.
           Yes I do. Yes I do. Yes I do. Yes I do. Yes I do. Yes I do.
         </p>
       </article>
       <article>
-        <h3>And I´m number three</h3>
+        <h3>And I&apos;m number three</h3>
         <p>
           And so do I. Yes I do. Yes I do. Yes I do. Yes I do. Yes I do. Yes I
           do. Yes I do. Yes I do.
