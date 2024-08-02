bc36e0109cf4f269e9935a2ecb3416bb7ef4fb4e
Rebecca Voll <doktavoll@gmail.com>
2024-08-02 22:52:47 +0200

    slight changes to 0529/react-basics_attributes/src/App.js

--- 0529_react-basics/react-basics_attributes/src/App.js
+++ 0529_react-basics/react-basics_attributes/src/App.js
@@ -8,7 +8,18 @@ export default function App() {
 function Article() {
   return (
     <article className="article">
-      <h2 className="article_title">some title</h2>
+      <h2 className="article_title">
+        Learning JSX, &quot;a syntax extension for JavaScript that lets you
+        write HTML-like markup inside a JavaScript file&quot; (
+        <a
+          className="title_link"
+          href="https://react.dev/learn/writing-markup-with-jsx"
+        >
+          https://react.dev/learn/writing-markup-with-jsx
+        </a>
+        )
+      </h2>
+
       <label id="input" htmlFor="input">
         fill in whatever you want:
       </label>
