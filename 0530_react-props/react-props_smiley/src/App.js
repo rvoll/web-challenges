727bcc2f7f37914ff7c4810e380617f34309fdc1
Rebecca Voll <doktavoll@gmail.com>
2024-08-03 16:27:59 +0200

    create component Smiley which renders happy or unhappy smileys depending on 2 props being passed down from the export default function App(): humptyIsHappy and dumptyIsHappy

--- 0530_react-props/react-props_smiley/src/App.js
+++ 0530_react-props/react-props_smiley/src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley humptyIsHappy />;
+}
+
+function Smiley({ humptyIsHappy, dumptyIsHappy }) {
+  return (
+    <div>
+      <p>Humpty: {humptyIsHappy ? "🥳" : "😭"}</p>
+      <p>Dumpty: {dumptyIsHappy ? "🥳" : "😭"}</p>
+    </div>
+  );
 }
