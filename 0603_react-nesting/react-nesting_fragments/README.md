adf7e9c01889e8c2e5521ae71920434542bf7508
Rebecca Voll <doktavoll@gmail.com>
2024-08-04 11:37:24 +0200

    create Boxes component, outsource the Boxes into it, replace <div> with fragments <> in 0603_react-nesting/react-nesting_fragments

--- 0603_react-nesting/react-nesting_fragments/README.md
+++ 0603_react-nesting/react-nesting_fragments/README.md
@@ -39,8 +39,6 @@ To work locally, please install the dependencies using `npm i` first.
 
 Run `npm run start` to start a development server and open the displayed URL in a browser.
 
-
-
 ### Scripts
 
 You can use the following commands:
--- 0603_react-nesting/react-nesting_fragments/src/App.js
+++ 0603_react-nesting/react-nesting_fragments/src/App.js
@@ -5,9 +5,27 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <main className="flex-container">
-      <Box color="#007bff" />
-      <Box color="#fc3" />
-      <Box color="#ff3333" />
+      <Boxes />
     </main>
   );
 }
+
+function Boxes() {
+  return (
+    <>
+      <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" />
+    </>
+  );
+}
+
+// Outsource the three Box components to a Boxes component.
+
+// In the App.js file, create a Boxes component which
+// returns the same three Box components as given above.
+// For now, use a <div> component as a wrapper in the
+// return statement of the Boxes component.
+
+// In the App component, replace the three
+// Box components with the Boxes component.
