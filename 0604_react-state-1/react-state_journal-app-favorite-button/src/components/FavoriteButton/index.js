80917447cc89e9e944ffade4cda95b9852a1b439
Rebecca Voll <doktavoll@gmail.com>
2024-08-05 20:51:37 +0200

    import useState, write setter function for variable isFavorite, write function toggleFavorite, call function upon Click event

--- 0604_react-state-1/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
+++ 0604_react-state-1/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
@@ -1,16 +1,22 @@
 import "./FavoriteButton.css";
 import { ReactComponent as StarFilled } from "./star-filled.svg";
 import { ReactComponent as Star } from "./star.svg";
+import { useState } from "react";
 
 export default function FavoriteButton() {
   // this should be a state variable
-  const isFavorite = false;
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  function toggleFavorite() {
+    setIsFavorite(!isFavorite);
+  }
 
   return (
     <button
       className="favorite-button"
       onClick={() => {
-        console.log("favorite button clicked");
+        toggleFavorite();
+        // console.log("favorite button clicked");
       }}
       aria-label="favorite"
     >
@@ -18,3 +24,11 @@ export default function FavoriteButton() {
     </button>
   );
 }
+
+// The component has an `isFavorite` boolean variable.
+// This variable is used to determine if the star icon
+// should be filled in or not.
+
+// - Make `isFavorite` a state variable.
+// - This state variable needs to be toggled when the user clicks the favorite button.
+// - Happy Hacking!
