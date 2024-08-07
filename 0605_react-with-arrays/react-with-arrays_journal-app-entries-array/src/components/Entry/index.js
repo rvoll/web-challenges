42408167b256426d57c77b09acf042bd8841defc
Rebecca Voll <doktavoll@gmail.com>
2024-08-07 11:09:15 +0200

    modify EntriesSection/index.js inserting an array and mapping over it, also modify Entries/index.js importing and inserting Fragment and Divider, Dividernot conditional yet

--- 0605_react-with-arrays/react-with-arrays_journal-app-entries-array/src/components/Entry/index.js
+++ 0605_react-with-arrays/react-with-arrays_journal-app-entries-array/src/components/Entry/index.js
@@ -1,19 +1,35 @@
 import FavoriteButton from "../FavoriteButton";
 import "./Entry.css";
+import Divider from "../Divider";
+import { Fragment } from "react";
 
-export default function Entry({ motto, notes, date }) {
+// last Question LEFT to DO:
+// >>> I think I must use Fragment and Divider in the ENTRY component!
+//
+// If you are using this template:
+// How can you render a <Divider /> component between each entry?
+// Don't break the flexbox layout (a Fragment might help)
+//
+// Avoid having a divider below the last or above the first entry
+
+// 😊 Now, the only thing left is to make the divider (see below!) conditional!
+
+export default function Entry({ motto, notes, date, id }) {
   return (
-    <article className="entry">
-      <time className="entry__date">{date}</time>
-      <div className="entry__content">
-        <div className="entry__motto-container">
-          <h2 className="entry__motto">
-            <q>{motto}</q>
-          </h2>
-          <FavoriteButton />
+    <Fragment key={id}>
+      <article className="entry">
+        <time className="entry__date">{date}</time>
+        <div className="entry__content">
+          <div className="entry__motto-container">
+            <h2 className="entry__motto">
+              <q>{motto}</q>
+            </h2>
+            <FavoriteButton />
+          </div>
+          <p className="entry__notes">{notes}</p>
         </div>
-        <p className="entry__notes">{notes}</p>
-      </div>
-    </article>
+      </article>
+      <Divider />
+    </Fragment>
   );
 }
